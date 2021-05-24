8b87c3478e538f16feb24d96a6115eb0ef3fd16a
rui-costa <costa.ruitiago@gmail.com>
2021-05-24 21:15:37 +0200

    correct token

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ MAIN_BRANCH="$3"
 CHANGELOG_FORMAT="$4"
 
 # FAIL for invalid version inputs
-if (( ${SEMVER} != 'MAJOR' && ${SEMVER} != 'MINOR' && ${SEMVER} != 'PATCH' ))
+if (( '${SEMVER}' != 'MAJOR' && '${SEMVER}' != 'MINOR' && '${SEMVER}' != 'PATCH' ))
 then
   echo "::group::ERROR: INVALID VERSION INPUT"
   echo "The version ${SEMVER} is not a valid value."
@@ -28,7 +28,7 @@ LATEST=`git tag | tail -1`
 # MAJOR version when you make incompatible API changes
 # Major version X (X.y.z | X > 0) MUST be incremented if any backwards incompatible changes are introduced to the public API
 
-if (( ${SEMVER} = 'MAJOR' ))
+if (( '${SEMVER}' = 'MAJOR' ))
 then
 MAJOR=`git tag | tail -1 | cut -d '.' -f 1 | awk '{$1=$1+1};1'`
 else
@@ -38,7 +38,7 @@ fi
 # MINOR version when you add functionality in a backwards compatible manner
 # Minor version Y (x.Y.z | x > 0) MUST be incremented if new, backwards compatible functionality is introduced to the public API.
 # It MUST be incremented if any public API functionality is marked as deprecated.
-if (( ${SEMVER} = 'MAJOR' ))
+if (( '${SEMVER}' = 'MAJOR' ))
 then
   MINOR=`git tag | tail -1 | cut -d '.' -f 2 | awk '{$1=$1+1};1'`
 else
@@ -47,7 +47,7 @@ fi
 
 # PATCH version when you make backwards compatible bug fixes.
 # Patch version Z (x.y.Z | x > 0) MUST be incremented if only backwards compatible bug fixes are introduced. A bug fix is defined as an internal change that fixes incorrect behavior.
-if (( ${SEMVER} = 'PATCH' ))
+if (( '${SEMVER}' = 'PATCH' ))
 then
   PATCH=`git tag | tail -1 | cut -d '.' -f 3 | awk '{$1=$1+1};1'`
 else
@@ -56,20 +56,20 @@ fi
 
 
 # Minor version MUST be reset to 0 when major version is incremented.
-if (( ${SEMVER} = 'MAJOR' ))
+if (( '${SEMVER}' = 'MAJOR' ))
 then
   MINOR='0'
 fi
 
 # Patch version MUST be reset to 0 when major version is incremented.
 # Patch version MUST be reset to 0 when minor version is incremented.
-if (( ${SEMVER} = 'MAJOR' || ${SEMVER} = 'MINOR' ))
+if (( '${SEMVER}' = 'MAJOR' || '${SEMVER}' = 'MINOR' ))
 then
   PATCH='0'
 fi
 
 # Add label if exists
-if (( ${LABEL} != '' ))
+if (( '${LABEL}' != '' ))
 then
 PATCH=${PATCH}-${LABEL}
 fi
@@ -89,7 +89,7 @@ cat data
 # https://docs.github.com/en/rest/reference/repos#create-a-release
 curl \
 -X POST \
--H 'Accept: application/vnd.github.v3+json' \
--H 'Authorization: Bearer $GITHUB_TOKEN' \
+-H "Accept: application/vnd.github.v3+json" \
+-H "Authorization: Bearer $GITHUB_TOKEN" \
 https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
 -d @data
