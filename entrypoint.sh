152d5cc09e5397cc90a0f75268c5346f1fc63fd0
rui-costa <costa.ruitiago@gmail.com>
2021-05-25 08:37:48 +0200

    add path to changelog

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,7 +82,7 @@ VERSION=$MAJOR.$MINOR.$PATCH
 
 # Generate the Changelog Text
 echo "{ \'tag_name\': \'$VERSION\', \'body\': \' ### Changelog\n\n" > data
-git log --all --pretty=format:$changelog $LATEST.. . | sed 's|*|-|g' >> data
+git log --all --pretty=format:$changelog -- $LATEST.. . | sed 's|*|-|g' >> data
 echo "\'" >> data
 
 cat data
